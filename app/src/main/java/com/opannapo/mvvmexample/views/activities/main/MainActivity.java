42e58c0b98373e08dedc899cfc6da03790d3dce6
Taufan <taufan.alfazri@gmail.com>
2020-08-05 00:55:21 +0700

    ButterKnife setup

--- app/src/main/java/com/opannapo/mvvmexample/views/activities/main/MainActivity.java
+++ app/src/main/java/com/opannapo/mvvmexample/views/activities/main/MainActivity.java
@@ -1,6 +1,7 @@
 package com.opannapo.mvvmexample.views.activities.main;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -9,11 +10,17 @@ import com.opannapo.core.layer.application.presenter.view.BaseActivity;
 import com.opannapo.mvvmexample.R;
 import com.opannapo.mvvmexample.entities.User;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
 /**
  * Created by napouser on 04,August,2020
  */
 public class MainActivity extends BaseActivity {
     MainVM vm;
+    @BindView(R.id.text)
+    TextView text;
 
     @Override
     protected int initLayout() {
@@ -22,6 +29,8 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreated(Bundle savedInstanceState) {
+        ButterKnife.bind(this);
+
         vm = new ViewModelProvider(this).get(MainVM.class);
         final Observer<User> liveUser = data -> {
             /*toolbarTransformer.setOnlineOrderCount(count);
