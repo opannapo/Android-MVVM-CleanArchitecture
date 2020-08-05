9da30f68444a075ab766718e0b4b0cf01e4638ea
Taufan <taufan.alfazri@gmail.com>
2020-08-05 15:10:23 +0700

    ViewPager setup & fragment member

--- app/src/main/java/com/opannapo/mvvmexample/views/activities/main/MainActivity.java
+++ app/src/main/java/com/opannapo/mvvmexample/views/activities/main/MainActivity.java
@@ -11,10 +11,17 @@ import androidx.viewpager.widget.ViewPager;
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.opannapo.core.layer.application.presenter.view.BaseActivity;
+import com.opannapo.core.layer.application.presenter.view.BaseFragment;
+import com.opannapo.core.layer.enterprise.utils.Log;
 import com.opannapo.mvvmexample.R;
+import com.opannapo.mvvmexample.views.adapter.ViewPagerAdapter;
+import com.opannapo.mvvmexample.views.fragments.HomeFragment;
+import com.opannapo.mvvmexample.views.fragments.ProfileFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
+import static com.opannapo.mvvmexample.R2.string.appbar_scrolling_view_behavior;
 
 /**
  * Created by napouser on 05,August,2020
@@ -33,6 +40,11 @@ public class MainActivity extends BaseActivity {
     @BindView(R.id.vpPages)
     ViewPager vpPages;
 
+    HomeFragment homeFragment;
+    ProfileFragment profileFragment;
+    ViewPagerAdapter vpAdapter;
+    BaseFragment currentFragment;
+
     @Override
     protected int initLayout() {
         return R.layout.activity_main;
@@ -41,6 +53,44 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreated(Bundle savedInstanceState) {
         ButterKnife.bind(this);
+        setupFragmentMember();
     }
 
+    private void setupFragmentMember() {
+        vpAdapter = new ViewPagerAdapter(this.getSupportFragmentManager(), appbar_scrolling_view_behavior);
+        homeFragment = new HomeFragment();
+        profileFragment = new ProfileFragment();
+
+        vpAdapter.addFragment(0, homeFragment);
+        vpAdapter.addFragment(1, profileFragment);
+        vpPages.setAdapter(vpAdapter);
+        vpPages.setOffscreenPageLimit(2);
+        vpPages.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                Log.i("ViewPager onPageScrolled, position: " + position);
+                switch (position) {
+                    case 0:
+                        currentFragment = homeFragment;
+                        break;
+                    case 1:
+                        currentFragment = profileFragment;
+                        break;
+                }
+                currentFragment.onHiddenChanged(false);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                Log.i("ViewPager onPageSelected, position: " + position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                Log.i("ViewPager onPageScrollStateChanged, state: " + state);
+            }
+        });
+
+
+    }
 }
---
+++ app/src/main/java/com/opannapo/mvvmexample/views/adapter/ViewPagerAdapter.java
@@ -0,0 +1,32 @@
+package com.opannapo.mvvmexample.views.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.opannapo.core.layer.application.presenter.view.BaseFragment;
+
+/**
+ * Created by napouser on 05,August,2020
+ */
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    private final BaseFragment[] fragments = new BaseFragment[2];
+
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @Override
+    public BaseFragment getItem(int position) {
+        return fragments[position];
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.length;
+    }
+
+    public void addFragment(int idx, BaseFragment val) {
+        fragments[idx] = val;
+    }
+}
